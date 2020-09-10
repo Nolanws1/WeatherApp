14371022df5ca9be199d874c252eb743f403ad87
Nolanws1 <Nolanws@gmail.com>
2020-09-09 18:27:58 -0700

    first push

---
+++ index.js
@@ -0,0 +1,28 @@
+var cities = ["Los Angeles", "Chicago", "New York"];
+
+for (var i = 0; i < cities.length; i++) {
+
+var searchedCity = document.createElement("BUTTON");
+var textn
+document.getElementById("buttons").append(searchedCity, cities[i]);
+
+}
+
+
+function renderButtons() {
+
+    
+        // var usersCity = document.getElementById("input").value;
+
+        // cities.push(usersCity)
+
+        
+
+       
+
+       
+
+
+
+    }
+
