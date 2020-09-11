38bc8c06dcac0a331f4a95a5ee44cd24e3b908f8
Nolanws1 <Nolanws@gmail.com>
2020-09-11 14:36:34 -0700

    edits

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
-var cities = ["New York", "Chicago", "Los Angeles"];
+var cities = [];
 
 
-function displayWeatherInfo() {
+function displayWeatherInfo(city) {
 
-    var city = $(this).attr("data-name");
-    var queryURL = "api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=34d64e7f0ea3fec2362c6a680ab02a2b"
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=34d64e7f0ea3fec2362c6a680ab02a2b"
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-
         console.log(response)
+        var temperature = response.main.temp
+        var fTemp = (temperature - 273.15) * 9 / 5 + 32
+        var cityName = response.name
+        localStorage.setItem("button",cityName)
+
+        $(".current-city").empty();
+        $(".current-city").append(`
+            <h2>${cityName}</h2>
+            <h3>Temperature: ${Math.round(fTemp)}°F</h2>
+            <h3>Humidity: ${response.main.humidity}%</h3>
+            <h3>Wind Speed: ${response.wind.speed}mph</h3>
+            <h3>UV Index ${response}:</h3> `)
     })
 }
 
 function renderCityButtons() {
-
     $("#buttons").empty();
     for (var i = 0; i < cities.length; i++) {
-
         var a = $("<button>");
         a.addClass("cityClass");
         a.attr("data-name", cities[i]);
@@ -31,18 +39,16 @@ function renderCityButtons() {
 $("#submit-city").on("click", function (event) {
     event.preventDefault();
     var city = $("#input").val().trim();
-
+    $("#input").val("");
     cities.push(city)
-
     renderCityButtons();
-    displayWeatherInfo();
+    displayWeatherInfo(city);
 })
 
 renderCityButtons();
 
+$("#buttons").on("click", "button", function () {
 
-
-
-
-
+    displayWeatherInfo($(this).text());
+})
 
